5fff055e387a55d6db5e1a83889f907f3008e011
Filip Pitak <filip.pitak@posam.sk>
2023-06-15 04:41:14 +0200

    Deployment edit

--- Infrastructure/setup/scripts/full_deploy.sh
+++ Infrastructure/setup/scripts/full_deploy.sh
@@ -8,18 +8,18 @@ TARGET=/etc/systemd/system
 
 cp $SERVICES_DIR/consul.service $TARGET/consul.service
 cp $SERVICES_DIR/listng-course-01.service $TARGET/listng-course-01.service
-cp $SERVICES_DIR/listng-course-02.service $TARGET/listng-course-02.service
+#cp $SERVICES_DIR/listng-course-02.service $TARGET/listng-course-02.service
 cp $SERVICES_DIR/listng-user-01.service $TARGET/listng-user-01.service
-cp $SERVICES_DIR/listng-user-02.service $TARGET/listng-user-02.service
+#cp $SERVICES_DIR/listng-user-02.service $TARGET/listng-user-02.service
 cp $SERVICES_DIR/listng-rest.service $TARGET/listng-rest.service
 cp $SERVICES_DIR/listng-ui.service $TARGET/listng-ui.service
 
 systemctl daemon-reload
 systemctl enable consul
 systemctl enable listng-course-01
-systemctl enable listng-course-02
+#systemctl enable listng-course-02
 systemctl enable listng-user-01
-systemctl enable listng-user-02
+#systemctl enable listng-user-02
 systemctl enable listng-rest
 systemctl enable listng-ui
 
